31333e3a02a2164b43f0e4ab79f33222c51ae485
emmaqbrown <eqbrown@isf.edu.hk>
2021-03-23 15:44:03 +0800

    delete solution

--- promotion.py
+++ promotion.py
@@ -12,24 +12,6 @@ def promotion():
 
     # 💻 YOUR CODE HERE
 
-    hsbc = Bank("HSBC")
-
-    for i in range(10):
-        account_name = "Account{}".format(i)
-        hsbc.add_account(account_name)
-        hsbc.deposit(account_name,i*10)
-
-    bisf = Bank("BISF")
-
-    for i in range(10):
-        account_name = "Account{}".format(i)
-        bal = hsbc.check_balance(account_name)
-        hsbc.withdraw(account_name,bal)
-
-        bisf.add_account(account_name)
-        bisf.deposit(account_name,bal)
-        bisf.deposit(account_name,bal) 
-
 
 if __name__ == "__main__":
-    part2()
+    promotion()
